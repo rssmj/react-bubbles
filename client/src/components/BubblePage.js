ef776cef27914cc2247c6e2b710f9ab296ca340d
Michael Ross <mross.j@gmail.com>
2020-02-28 13:21:52 -0500

    render BubblePage, make GET request
    * set GET request to fetch color data for BubblePage
    * set PUT request to update saveEdit
    * set DELETE request to update deleteColor

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,33 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+	const [colorList, setColorList] = useState([]);
+	// fetch your colors data from the server when the component mounts
+	// set that data to the colorList state property
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+	useEffect(() => {
+		axiosWithAuth()
+			.get('/colors')
+			.then(res => {
+				console.table(res.data);
+				console.log(res.data);
+				setColorList(res.data);
+			})
+			.catch(err => {
+				console.log(`You got no colors!`, err.response);
+			});
+	}, []);
+
+	return (
+		<>
+			<ColorList colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</>
+	);
 };
 
 export default BubblePage;
